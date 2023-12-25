30edbb2ee9ddb08a8387580e4bfed99142bc31c8
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-25 14:00:51 -0500

    added some more ui customisation to the game

--- tictactoeai.py
+++ tictactoeai.py
@@ -2,11 +2,8 @@
 from tkinter import *
 import random
 
-def drawBoard(board):
-    pass
-
-def nextMove(board, player):
-    pass
+def nextMove(row, col):
+    global player
 
 def checkWinner(board):
     pass
@@ -17,6 +14,8 @@ def checkEmptySpaces(board):
 def newGame():
     pass
 
+window = Tk()
+window.title("Tic Tac Toe AI")
 
 players = ["x", "o"]
 player = random.choice(players)
@@ -25,8 +24,20 @@ buttons = [[0,0,0],
            [0,0,0]]
 
 
-window = Tk()
-window.title("Tic Tac Toe AI")
+
+label = Label(text= player + " turn", font=("Helvetica", 40))
+label.pack(side="top")
+
+resetButton = Button(text="New Game", font=("Helvetica", 32), command=newGame)
+resetButton.pack(side="top")
+
+frame = Frame(window)
+frame.pack()
+
+for row in range(3):
+    for col in range(3):
+        buttons[row][col] = Button(frame, text="", font=("Helvetica", 32), width=5, height=2, command=lambda row=row, col=col: nextMove(row, col))
+        buttons[row][col].grid(row=row, column=col)
+
 window.geometry("700x700")
 window.mainloop()
-
